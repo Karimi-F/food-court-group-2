48e5c4218ae2f3fd780b3740c42594df42e0c68a
Sam Tomashi <samtomashikayembi@yahoo.fr>
2025-02-20 13:49:17 +0300

    captured logged in user and redirect unauthorized access to login page

--- src/app/owner-dashboard/page.js
+++ src/app/owner-dashboard/page.js
@@ -1,11 +1,23 @@
 "use client"
 import { useSession } from "next-auth/react"
+import { useRouter } from "next/navigation"
+import { useEffect } from "react"
 
-export default function Dashboard(){
-    const {data, status}= useSession()
-    return(
-    <>
-    <h1>Hi {data?.user?.name}, <br></br>welcome to your dashboard</h1>
-    </>
-    )   
+export default function Dashboard() {
+    const { data: session, status } = useSession();
+    const router = useRouter();
+
+    useEffect(() => {
+        if (status === "unauthenticated") {
+            router.push("/owner-login"); // Redirect to login page
+        }
+    }, [status, router]);
+
+    if (status === "loading") return <p>Loading...</p>;
+
+    return (
+        <>
+            <h1>Hi {session?.user?.name}, <br />Welcome to your dashboard</h1>
+        </>
+    );
 }
